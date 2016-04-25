3749a72e4e1b8ff7bbd9ffc126673352d40e96a0
April Liu <liux2003@umn.edu>
2016-04-24 19:05:30 -0700

    Create frequency.py

---
+++ assignment1/frequency.py
@@ -0,0 +1,46 @@
+import json
+import sys
+reload(sys)
+sys.setdefaultencoding("utf-8")
+
+def hw():
+    print 'Hello, world!'
+
+def lines(fp):
+    print str(len(fp.readlines()))
+
+def main():
+    
+    tweet_file = open(sys.argv[1])
+
+    twitterlist = []  # Create list with dictionary
+    for line in tweet_file:
+    	line = line.encode('utf-8')
+    	twitterlist.append(json.loads(line)) 
+    
+    
+    count_word = {}
+    count_total = {}
+    for i in range(len(twitterlist)):
+    	if "text" in twitterlist[i]:
+    		tweetword = twitterlist[i]["text"].split()
+    		sentscore = 0
+    		for word in tweetword:
+    			word = word.encode('utf-8')
+    			if word not in count_word:
+    				count_word[word] = 1
+    			elif word in count_word:
+    				count_word[word] += 1
+    			count_total[word] = sum(len(x) for x in tweetword if word in x)
+    				
+	d = {}
+	for key, val in count_word.items():
+		d[key] = val/count_total[key]
+    		
+    for k, v in d.items():
+    	print k, float(v)
+    
+    		
+    
+if __name__ == '__main__':
+	main()
