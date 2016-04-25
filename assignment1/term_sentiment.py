1bddcf6ef3f4e084f440f2ded9a3a85fb30fecac
April Liu <liux2003@umn.edu>
2016-04-24 17:16:11 -0700

    Update term_sentiment.py

--- assignment1/term_sentiment.py
+++ assignment1/term_sentiment.py
@@ -1,3 +1,7 @@
+''' first methodology: define sentiment metric score as positive_count/neg_count but the result is not correct'''
+
+'''first methodology''' 
+
 import json
 import sys
 
@@ -51,6 +55,68 @@ def main():
 			d[key] = float(scores_others_pos[key])/val
 		elif val is 0:
 			d[key] = float(scores_others_pos[key])
+    		
+    for k, v in d.items():
+    	print k, float(v)
+    
+    		
+    
+if __name__ == '__main__':
+	main()
+	
+'''second methodology: sentiment score definition is sourced from http://www.slideshare.net/faigg/tutotial-of-sentiment-analysis'''
+'''for example, if the word 'football' exists in two tweets, the first tweet sentiment score is 5, the second tweet sentiment score is 1
+then the sentiment score for the word 'football' should be (5+1)/2 = 3'''
+'''to achieve this, my thought is to create two differnt dictionaries: one is senti_sore to store each word and their total score; another 
+is the number of the tweets the word is in''' 
+
+'''second methodology:'''
+import json
+import sys
+
+def hw():
+    print 'Hello, world!'
+
+def lines(fp):
+    print str(len(fp.readlines()))
+
+def main():
+    sent_file = open(sys.argv[1])
+    tweet_file = open(sys.argv[2])
+
+    twitterlist = []  # Create list with dictionary
+    for line in tweet_file:
+    	line = line.encode('utf-8')
+    	twitterlist.append(json.loads(line)) 
+    
+    scores = {} # initialize an empty dictionary
+    for line in sent_file:
+    	term, score  = line.split("\t")  # The file is tab-delimited. "\t" means "tab character"
+    	scores[term] = int(score)  # Convert the score to an integer.
+    
+    sent_score = {}
+    count_number = {}
+    for i in range(len(twitterlist)):
+    	if "text" in twitterlist[i]:
+    		tweetword = twitterlist[i]["text"].split()
+    		sentscore = 0
+    		for word in tweetword:
+    			word = word.encode('utf-8')
+    			if word in scores:
+    				sentscore = sentscore + scores[word]
+    		for word in tweetword:	
+    			word = word.encode('utf-8')
+    			if word not in scores:
+    				if word not in sent_score:
+    					sent_score[word] = sentscore
+    					count_number[word] = 1
+    				elif word in sent_score:
+    					sent_score[word] = sentscore + sent_score[word]
+    					count_number[word] = count_number[word] + 1
+    				
+    d = {}
+    for key, val in sent_score.items():
+	d[key] = float(val)/count_number[key]
     		
     for k, v in d.items():
     	print k, float(v)
